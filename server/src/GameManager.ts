ae787d9034c4d8fa16a7a6b628f7a882fe88a1fe
casluminous <stealla501@gmail.com>
2026-07-02 14:36:37 +0700

    fix: remove dead player from room after shot to prevent extra trigger

--- server/src/GameManager.ts
+++ server/src/GameManager.ts
@@ -348,6 +348,11 @@ export class GameManager {
       targetPlayer.isAlive = false;
       targetPlayer.hasCards = false;
 
+      const deadSocket = this.io.sockets.sockets.get(targetPlayer.id);
+      if (deadSocket) {
+        deadSocket.leave(roomId);
+      }
+
       this.io.to(roomId).emit('game:trigger', {
         alive: false,
         playerId: targetPlayer.id,
